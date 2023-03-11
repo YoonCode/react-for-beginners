d5644f4c57f2d81cd60a7daaedc803fc7835614e
coder.yoon <yjin830201@gmail.com>
2023-03-11 17:02:20 +0900

    coin converter

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [input, setInput] = useState("");
+  const [converted, setConverted] = useState("");
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then(res => res.json())
@@ -11,20 +13,39 @@ function App() {
         setLoading(false);
       });
   }, []);
+  const onChange = event => {
+    setInput(() => event.target.value);
+  };
+  const onSelect = event => {
+    const btc = coins.find(coin => coin.name === event.target.value);
+    setConverted(prev => prev / btc);
+  };
   return (
     <>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onSelect}>
           {coins.map(coin => (
-            <option key={coin.id}>
+            <option key={coin.id} value={coin.quotes.USD.price}>
               {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+      <hr />
+      <div>
+        <label htmlFor="coin">Coin</label>
+        <input
+          onChange={onChange}
+          id="coin"
+          type="number"
+          value={input}
+          placeholder="Input here..."
+        />
+        <strong> = {converted === "" ? null : converted} BTC</strong>
+      </div>
     </>
   );
 }
