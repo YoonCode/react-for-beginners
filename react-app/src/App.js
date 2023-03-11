39b6ca2d81c7506019890b492fedd4878e380406
coder.yoon <yjin830201@gmail.com>
2023-03-11 21:22:31 +0900

    add routes, components folders and move code

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,42 +1,5 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map(movie => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt={movie.title} />
-              <h3>{movie.title}</h3>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map(genre => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </>
-  );
+  return null;
 }
 
 export default App;
